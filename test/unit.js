e4bfbcfc8b0b4c894c37b5fb3f811d8b6688c453
Joshua Nelson <jonelson@atlassian.com>
2016-05-03 17:01:56 +1000

    Update to use the latest skatejs/build

--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,1 @@
-//This only works because we're transpiling ES6 modules. There's an implicit dependency between setup and react-wc-tests
-//We need this because we need to set up a global on the window *before* react starts initialising, but we cannot modify
-//react to depend on the setup code. The alternate solution is to modify the build so that the setup isn't necessary.
-import './setup';
 import './react-wc-tests';
