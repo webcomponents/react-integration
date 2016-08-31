a9d53f59a9be625065c29c3462bb386ecd8461cd
Steve Mao <maochenyan@gmail.com>
2016-08-31 10:11:01 +1000

    do not bundle react and react-dom

--- rollup.config.js
+++ rollup.config.js
@@ -1,1 +1,13 @@
-module.exports = require('skatejs-build/rollup.config');
+const config = require('skatejs-build/rollup.config');
+const pkg = require('skatejs-build/package');
+
+const deps = Object.keys(pkg.dependencies || []);
+config.external = id => {
+  if (id === 'react' || id === 'react-dom') {
+    return true;
+  }
+
+  return deps.indexOf(id) > -1
+}
+
+module.exports = config;
