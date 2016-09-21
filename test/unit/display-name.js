faf740e6ab2825ac23158efdcfcfed6389f3a523
Tim Sebastian <tim.sebastian@gmail.com>
2016-09-21 13:21:42 +0200

    it should read PascalCased not camelCased

--- test/unit/display-name.js
+++ test/unit/display-name.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 describe('display-name', () => {
-  it('should be a camelCased version of the tagName', () => {
+  it('should be a PasalCased version of the tagName', () => {
     const Comp = reactify(document.registerElement('x-display-name-1', {
       prototype: Object.create(HTMLElement.prototype),
     }), { React, ReactDOM });
