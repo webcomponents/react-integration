7507beb24e73e17c7d8c3fab40127b1d61194136
Bradley Ayers <bradley.ayers@gmail.com>
2016-11-09 21:51:09 +1100

    fix: Add TypeScript declarations.

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+
+type CustomElementCtor = { new(...args: any[]): HTMLElement };
+type ReactComponentCtor = { new(...args: any[]): React.Component<any, any> };
+type Options = { React?: any, ReactDOM?: any };
+export default function (CustomElement: CustomElementCtor, opts: Options = {}): ReactComponentCtor;
