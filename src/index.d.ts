da9620920f92907de8f8cf93fd31ac129f97a381
Bradley Ayers <bradley.ayers@gmail.com>
2017-01-17 20:03:32 +1100

    fix: Fix TypeScript declaration

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,4 +3,4 @@ import * as React from 'react';
 type CustomElementCtor = { new(...args: any[]): HTMLElement };
 type ReactComponentCtor = { new(...args: any[]): React.Component<any, any> };
 type Options = { React?: any, ReactDOM?: any };
-export default function (CustomElement: CustomElementCtor, opts: Options = {}): ReactComponentCtor
+export default function (CustomElement: CustomElementCtor, opts?: Options): ReactComponentCtor
